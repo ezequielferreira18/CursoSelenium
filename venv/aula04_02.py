4f6c31de2a99cdeb21555a99999f1990e331ebc0
Ezequiel Machado Ferreira <ezequielmmf18@gmail.com>
2022-10-13 16:50:33 -0300

    aula04_02.py.py

--- venv/aula04_02.py
+++ venv/aula04_02.py
@@ -12,12 +12,24 @@ def find_by_text(driver, tag, text):
 
     Argumentos:
      - driver: Instância do browser (Chrome)
-     - text:  conteúdo que estará na tag
-     - tag: onde o texto será buscado
+     - text: Conteúdo que estará na tag
+     - tag: Onde o texto será buscado
     """
     elements = driver.find_elements(By.TAG_NAME, tag) # Retorna lista, pois há vários elementos com a mesma TAG_NAME
     for e in elements:
         if e.text == text:
+            return e
+
+def find_by_href(driver, link):
+    """ --Encontrar o elemento 'a' com o link 'link'
+
+    Argumentos:
+     - driver: Instância do browser (Chrome)
+     - link: Link que será procurado dentro de todas as tag 'a'
+    """
+    elements = driver.find_elements(By.TAG_NAME, 'a')
+    for e in elements:
+        if link in e.get_attribute('href'):
             return e
 
 driver.get('http://selenium.dunossauro.live/aula_04_a.html')
@@ -25,3 +37,9 @@ driver.get('http://selenium.dunossauro.live/aula_04_a.html')
 element_ddg = find_by_text(driver, 'li', 'DuckDuckGo')
 print(element_ddg)
 print(element_ddg.text)
+print(element_ddg.get_attribute('href'))
+
+element_ddg = find_by_href(driver, 'ddg.gg')
+print(element_ddg)
+print(element_ddg.text)
+print(element_ddg.get_attribute('href'))
